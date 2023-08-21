7ae26faa7227085b64e569161a72faf1ba4d804b
adamsaunders26 <a.saunders206@gmail.com>
2023-08-21 21:20:51 +0100

    change to POST route handler

--- app/API/contact/route.ts
+++ app/API/contact/route.ts
@@ -3,17 +3,15 @@ import EmailSend from "./EmailSend";
 import { formState } from "@/app/contact/page";
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const { method } = req;
+  //   const { method } = req;
   const { formData }: { formData: formState } = await req.json();
   const body = `Message from ${formData.name} via ${formData.email} on Portfolio site.
   
 Message: ${formData.message}`;
 
-  if (method === "POST") {
-    console.log("u made it bro!");
-    await EmailSend(formData.subject, body);
-    return NextResponse.json({ msg: "success" }, { status: 200 });
-  }
+  console.log("u made it bro!");
+  await EmailSend(formData.subject, body);
+  return NextResponse.json({ msg: "success" }, { status: 200 });
 }
 
 export async function GET() {
