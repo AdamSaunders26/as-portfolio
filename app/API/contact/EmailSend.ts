c776445c2c744975ae418366f0bc88e7c7391f1e
adamsaunders26 <a.saunders206@gmail.com>
2023-08-21 17:46:23 +0100

    fixed issue with npm update, added email functionality to contact form

--- app/API/contact/EmailSend.ts
+++ app/API/contact/EmailSend.ts
@@ -1,27 +1,28 @@
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
-// export default async function EmailSend(subject, body) {
-//   const transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
-//       pass: process.env.NEXT_PUBLIC_NODEMAILER_PW,
-//     },
-//   });
+export default async function EmailSend(subject: string, body: string) {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
+      pass: process.env.NEXT_PUBLIC_NODEMAILER_PW,
+    },
+  });
 
-//   const mailOptions = {
-//     from: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
-//     to: "contact@adam-saunders.dev",
-//     subject: subject,
-//     text: body,
-//   };
-
-//   transporter.sendMail(mailOptions, (error, info) => {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log("Email sent");
-//       return true;
-//     }
-//   });
-// }
+  const mailOptions = {
+    from: process.env.NEXT_PUBLIC_NODEMAILER_EMAIL,
+    to: "contact@adam-saunders.dev",
+    subject: subject,
+    text: body,
+  };
+  await new Promise((resolve, reject) => {
+    return transporter.sendMail(mailOptions, (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        console.log("Email sent");
+        resolve(response);
+      }
+    });
+  });
+}
--- app/API/contact/route.ts
+++ app/API/contact/route.ts
@@ -1,11 +1,16 @@
-// import EmailSend from "./EmailSend";
+import { NextRequest, NextResponse } from "next/server";
+import EmailSend from "./EmailSend";
+import { formState } from "@/app/contact/page";
 
-// export default function ContactAPI() {
-//   async function handler(req, res) {
-//     const { method } = req;
-//     if (method === "POST") {
-//       await EmailSend("Funny subject", "email body");
-//       res.status(200).send("Success");
-//     }
-//   }
-// }
+export async function POST(req: NextRequest, res: NextResponse) {
+  const { method } = req;
+  const { formData }: { formData: formState } = await req.json();
+  const body = `Message from ${formData.name} via ${formData.email} on Portfolio site.
+  
+Message: ${formData.message}`;
+
+  if (method === "POST") {
+    await EmailSend(formData.subject, body);
+    return NextResponse.json({ msg: "success" }, { status: 200 });
+  }
+}
