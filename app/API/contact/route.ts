c457d87681a52e37becaf888f38848f671b025cc
adamsaunders26 <a.saunders206@gmail.com>
2023-08-21 21:27:27 +0100

    add header function to next config

--- app/API/contact/route.ts
+++ app/API/contact/route.ts
@@ -5,6 +5,7 @@ import { formState } from "@/app/contact/page";
 export async function POST(req: NextRequest, res: NextResponse) {
   //   const { method } = req;
   const { formData }: { formData: formState } = await req.json();
+  console.log(formData);
   const body = `Message from ${formData.name} via ${formData.email} on Portfolio site.
   
 Message: ${formData.message}`;
