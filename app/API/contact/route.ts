afdae605ceb6bbdc9244157846acbd71f6ae6cef
adamsaunders26 <a.saunders206@gmail.com>
2023-08-21 20:53:27 +0100

    update emailSend import again

--- app/API/contact/route.ts
+++ app/API/contact/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import EmailSend from "@/app/api/contact/EmailSend";
 import { formState } from "@/app/contact/page";
+import EmailSend from "./EmailSend";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   const { method } = req;
