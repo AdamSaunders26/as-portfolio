10ddf1733c2368b970d5d0095bb5fa4e3e0cb6a6
adamsaunders26 <a.saunders206@gmail.com>
2023-10-16 22:34:52 +0100

    redesign grid layout on 'about' page

--- app/FeaturedProject.tsx
+++ app/FeaturedProject.tsx
@@ -2,30 +2,35 @@ import Image from "next/image";
 import classroomToolkitImg from "../images/classroomToolkit-FP.png";
 export default function FeaturedProject() {
   return (
-    <article className=" mt-4 h-fit rounded-md px-4 py-2 dark:bg-black">
+    <article className=" col-span-3 mt-4 h-fit w-fit place-self-center rounded-md bg-white px-4 py-2 dark:bg-black">
       <h3 className="text-xl">
         Currently working on:{" "}
         <span className="text-sky-700 dark:text-sky-500">
           Classroom Toolkit
         </span>
       </h3>
-      <div className="flex  gap-4 ">
+      <div className="flex   flex-col gap-4 sm:flex-row ">
         <Image
           src={classroomToolkitImg}
-          className="max-h-[16rem] w-fit rounded-xl p-2"
+          className="w-fit rounded-xl p-2 sm:max-h-[10rem] lg:max-h-[16rem]"
           priority={true}
           placeholder="blur"
           alt="A screenshot of the classes page on Classroom-toolkit.co.uk"
         />
-        <p>
-          Classroom Toolkit is a site aimed at teachers to help make their lives
-          easier. It's build using Next.js and Typescript, using Next's API
-          route handlers as the back-end API which is linked to a PostgreSQL
-          database using Prisma. It currently only functions as a simple class
-          manager. Next up will be a selection of customisable tracker which
-          could be used for keepinb track of which pupils have completed
-          homework or as a behaviour record.
-        </p>
+        <div className="flex max-w-[30rem] flex-col justify-between">
+          <p className="">
+            Classroom Toolkit is a site aimed at teachers to help make their
+            lives easier. It's build using Next.js and Typescript, using Next's
+            API route handlers as the back-end API which is linked to a
+            PostgreSQL database using Prisma. It currently only functions as a
+            simple class manager. Next up will be a selection of customisable
+            tracker which could be used for keeping track of which pupils have
+            completed homework or as a behaviour record.
+          </p>
+          <button className="place-self-end rounded-md bg-sky-600 p-2 text-white ">
+            Visit project page
+          </button>
+        </div>
       </div>
     </article>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import FeaturedProject from "./FeaturedProject";
 
 export default function Home() {
   return (
-    <main className=" mx-4  mt-24 grid h-full grid-cols-1 items-center sm:mt-16  sm:grid-cols-3">
+    <main className=" mx-4  mt-24 grid h-full grid-cols-1 items-center sm:mt-16  sm:grid-cols-3 ">
       <div className="mx-4 mb-2 flex  flex-col">
         <Image
           className="  my-4 h-64 w-64 place-self-center rounded-full object-cover sm:max-h-[36rem]"
@@ -19,42 +19,42 @@ export default function Home() {
           alt="Adam smiling and looking into the camera"
         />
       </div>
-      <div className="col-span-2 grid max-h-full grid-rows-2">
-        <article className="mt-4 flex h-max w-full flex-col justify-center place-self-center rounded-md bg-white px-4 py-2 dark:bg-black sm:mt-4">
-          <div className="border-b-2 border-sky-600">
-            <h2 className="text-2xl font-bold text-sky-700 dark:text-sky-500">
-              Adam Saunders <Pronouns subject="he" object="him" />
-            </h2>
-            <h3 className="mb-2 text-xl font-bold  ">Software Developer</h3>
-          </div>
-          <p className="mt-2 border-b-2 border-sky-600 pb-2">
-            Recent{" "}
-            <a
-              className="text-sky-700 dark:text-sky-500 "
-              target="_blank"
-              href="https://northcoders.com/"
-            >
-              Northcoders
-            </a>{" "}
-            graduate coding in JavaScript who enjoys working in a team and
-            solving problems through code.
-          </p>
-          <p className="mt-2">
-            After having trained as a primary school teacher and working with 30
-            excitable children each day, programming is a breath of fresh air.
-            Although I have many fond memories of the classes I've taught and
-            schools I've worked at, coding has gotten me excited to get out of
-            bed in the mornings again. I love the constant battle between taking
-            on tricky challenges and then actually finding solutions for them.
-            This portfolio website is the first thing I have created since
-            leaving the Northcoders course - I hope you enjoy browsing through
-            the site. Please don't hesitate to get in touch if you have a great
-            idea you'd like to collaborate on or if you think I'd be a good fit
-            for a vacancy at your company.
-          </p>
-        </article>
-        <FeaturedProject />
-      </div>
+      {/* <div className="col-span-2 sm:grid sm:grid-rows-2"> */}
+      <article className="col-span-2 mt-4 flex h-max w-full flex-col justify-center place-self-center rounded-md bg-white px-4 py-2 dark:bg-black sm:mt-4">
+        <div className="border-b-2 border-sky-600">
+          <h2 className="text-2xl font-bold text-sky-700 dark:text-sky-500">
+            Adam Saunders <Pronouns subject="he" object="him" />
+          </h2>
+          <h3 className="mb-2 text-xl font-bold  ">Software Developer</h3>
+        </div>
+        <p className="mt-2 border-b-2 border-sky-600 pb-2">
+          Recent{" "}
+          <a
+            className="text-sky-700 dark:text-sky-500 "
+            target="_blank"
+            href="https://northcoders.com/"
+          >
+            Northcoders
+          </a>{" "}
+          graduate coding in JavaScript who enjoys working in a team and solving
+          problems through code.
+        </p>
+        <p className="mt-2">
+          After having trained as a primary school teacher and working with 30
+          excitable children each day, programming is a breath of fresh air.
+          Although I have many fond memories of the classes I've taught and
+          schools I've worked at, coding has gotten me excited to get out of bed
+          in the mornings again. I love the constant battle between taking on
+          tricky challenges and then actually finding solutions for them. This
+          portfolio website is the first thing I have created since leaving the
+          Northcoders course - I hope you enjoy browsing through the site.
+          Please don't hesitate to get in touch if you have a great idea you'd
+          like to collaborate on or if you think I'd be a good fit for a vacancy
+          at your company.
+        </p>
+      </article>
+      <FeaturedProject />
+      {/* </div> */}
       <Socials horizontal={true} cv={true} />
     </main>
   );
